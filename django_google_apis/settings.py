7421cf0ecd4cd6ead9eaa8c453cbf7a006919b7a
Wasini <grazini.rodrigo@gmail.com>
2022-04-21 18:40:08 -0300

    add htmls templates add templates dir in settings

--- django_google_apis/settings.py
+++ django_google_apis/settings.py
@@ -67,7 +67,9 @@ ROOT_URLCONF = 'django_google_apis.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
